02035b7303a3b6f8d1bd8199ce4c37100a98aaf6
Gibson Bell <95596261+gbyo@users.noreply.github.com>
2026-09-20 20:25:34 -0400

    Format OpenAPI ref resolver

--- apps/api/src/app.ts
+++ apps/api/src/app.ts
@@ -168,7 +168,10 @@ export async function createApp(options: CreateAppOptions): Promise<FastifyInsta
     },
     refResolver: {
       buildLocalReference(json, _baseUri, _fragment, index) {
-        return typeof json.$id === 'string' && json.$id.length > 0 ? json.$id : `def-${String(index)}`;
+        if (typeof json.$id === 'string' && json.$id.length > 0) {
+          return json.$id;
+        }
+        return `def-${String(index)}`;
       },
     },
   });
