e539b2a600df1b1226a230aa52c26485a5d5bb91
Gibson Bell <95596261+gbyo@users.noreply.github.com>
2026-09-21 07:50:01 -0400

    Format transaction settings type

--- packages/application/src/persistence.ts
+++ packages/application/src/persistence.ts
@@ -39,7 +39,11 @@ export interface SystemTransactionContext {
 export type TransactionContext = TenantTransactionContext;
 
 export interface TenantTransactionSettings {
-  readonly isolationLevel?: 'read uncommitted' | 'read committed' | 'repeatable read' | 'serializable';
+  readonly isolationLevel?:
+    | 'read uncommitted'
+    | 'read committed'
+    | 'repeatable read'
+    | 'serializable';
   readonly accessMode?: 'read only' | 'read write';
 }
 
